d7e5e0ee165feaf579a738bb35156141fe9e34c8
Sven <steve.vernyi@gmail.com>
2023-12-11 01:36:01 -0600

    Adds root QPdf object for interacting with PDFs with read functionality

---
+++ src/QPdfSharp/QPdf.cs
@@ -0,0 +1,60 @@
+﻿// Copyright © Stephen (Sven) Vernyi and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+using System.Text;
+using QPdfSharp.Interop;
+
+namespace QPdfSharp;
+
+public unsafe class QPdf: IDisposable
+{
+    private readonly QPdfData* _qPdfData = QPdfInterop.qpdf_init();
+
+    ~QPdf() => ReleaseUnmanagedResources();
+
+    public QPdf(string filePath, string password = "")
+    {
+        fixed (byte* filePathBytes = Encoding.UTF8.GetBytes(filePath))
+        fixed (byte* passwordBytes = Encoding.UTF8.GetBytes(password))
+        {
+            var readResult = QPdfInterop.qpdf_read(_qPdfData, (sbyte*)filePathBytes, (sbyte*)passwordBytes);
+            CheckError(readResult);
+        }
+    }
+
+    public QPdf(ref ReadOnlyMemory<byte> bytes, string name = "", string password = "")
+    {
+        using var fileBytesHandle = bytes.Pin();
+
+        fixed (byte* fileNameBytes = Encoding.UTF8.GetBytes(name))
+        fixed (byte* passwordBytes = Encoding.UTF8.GetBytes(password))
+        {
+            var readResult = QPdfInterop.qpdf_read_memory(_qPdfData, (sbyte*)fileNameBytes, (sbyte*)fileBytesHandle.Pointer, (ulong)bytes.Length, (sbyte*)passwordBytes);
+            CheckError(readResult);
+        }
+    }
+
+    public void Dispose()
+    {
+        ReleaseUnmanagedResources();
+        GC.SuppressFinalize(this);
+    }
+
+    private void CheckError(int? errorCode = null)
+    {
+        errorCode ??= QPdfInterop.qpdf_has_error(_qPdfData);
+
+        if (errorCode == 0)
+            return;
+
+        var error = QPdfInterop.qpdf_get_error(_qPdfData);
+        var errorMessage = new string(QPdfInterop.qpdf_get_error_full_text(_qPdfData, error));
+
+        throw new QPdfException(errorMessage);
+    }
+
+    private void ReleaseUnmanagedResources()
+    {
+        fixed (QPdfData** pdf = &_qPdfData)
+            QPdfInterop.qpdf_cleanup(pdf);
+    }
+}
---
+++ src/QPdfSharp/QPdfException.cs
@@ -0,0 +1,5 @@
+﻿// Copyright © Stephen (Sven) Vernyi and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+namespace QPdfSharp;
+
+public class QPdfException(string message) : Exception(message);
